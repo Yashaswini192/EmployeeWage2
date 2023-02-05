c33226264a28850f2e62070b1288cf5c68df641d
Yashaswini192 <voruganti.yashaswini@ust.com>
2023-02-05 21:56:18 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,38 @@
+package com.bl;
+
+public class Main {
+	
+    public static void main(String[] args) {
+    	 int isPresent = 1;
+    	 int isAbsent = 0;
+    	 int daycount=0;
+    	 int salaryPerHour = 20;
+         int partTime=2;
+    	 //workingDaysPerMonth=20
+    	 int EmpMonthlySalary; 
+    	 int WorkingHours=0;
+         int totalWorkingHours;
+        
+         int checkAttendance = (int) ((Math.random() * 10) % 3);
+         if(checkAttendance == isPresent) {
+        	System.out.println("employee is present");
+        	daycount=20;
+        	WorkingHours=160;
+        }else if (checkAttendance == partTime) {
+        	System.out.println("employee is working part time");
+        	daycount=10;
+        	WorkingHours=80;
+        }else if(checkAttendance == isAbsent){
+        	System.out.println("employee is absent");
+        }else {
+        	System.out.println("no employee is working in this position");
+        }
+        totalWorkingHours = daycount *  WorkingHours ; 
+        System.out.println(" workingHours:"+WorkingHours);
+        int dailySalary= WorkingHours * salaryPerHour;
+        System.out.println("DailySalary is:"+dailySalary);
+        EmpMonthlySalary = totalWorkingHours * dailySalary ;
+        System.out.println("Employee monthly Salary is :"+ EmpMonthlySalary);
+     }
+  }
+
