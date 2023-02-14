d57c964f53b51bca46179d7448526a49c8175e92
Yashaswini192 <voruganti.yashaswini@ust.com>
2023-02-14 16:46:08 +0530

    refactor the code into classes and methods

---
+++ EmpWage.java
@@ -0,0 +1,55 @@
+package com.bl;
+
+public class EmpWage {
+
+	static int isPresent = 1 ;
+	static int isAbsent = 0 ;
+	static int part_time = 2;
+	static int wagePerHour = 20;
+	static int fulldayHours = 20;
+	static int workingDaysPerMonth = 20;
+	static int totalWorkingHours = 0;
+	static int WorkingHours = 0;
+
+	public void Attendance() {
+
+		int checkAttendance = (int) ((Math.random() * 10) % 4);
+		switch (checkAttendance) {
+		case 1:
+			System.out.println("employee is present");
+			WorkingHours = 100;
+			workingDaysPerMonth = 20;
+			break;
+		case 2:
+			System.out.println("employee is absent");
+			WorkingHours = 0;
+			workingDaysPerMonth = 0;
+			break;
+		case 3:
+			System.out.println("employee is working part time");
+			WorkingHours = 50;
+			workingDaysPerMonth = 10;
+			break;
+		default:
+			System.out.println(" employee need to be hired");
+			break;
+		}
+	}				
+}
+
+class Dailywages extends EmpWage{ //single Inheritance
+
+
+	public void dailywage() {
+
+		totalWorkingHours = workingDaysPerMonth *  WorkingHours ; 
+		System.out.println("totalworkingHours:"+totalWorkingHours);
+
+		int dailyWages = WorkingHours * wagePerHour ;
+		System.out.println("dailywages:"+dailyWages);
+
+		int MonthlyWages = totalWorkingHours * dailyWages ;
+		System.out.println("Employee monthly wages is :" +MonthlyWages);
+
+	}
+}
--- Main.java
+++ Main.java
@@ -1,38 +1,13 @@
 package com.bl;
 
-public class Main {
-	
-    public static void main(String[] args) {
-    	 int isPresent = 1;
-    	 int isAbsent = 0;
-    	 int daycount=0;
-    	 int salaryPerHour = 20;
-         int partTime=2;
-    	 //workingDaysPerMonth=20
-    	 int EmpMonthlySalary; 
-    	 int WorkingHours=0;
-         int totalWorkingHours;
-        
-         int checkAttendance = (int) ((Math.random() * 10) % 3);
-         if(checkAttendance == isPresent) {
-        	System.out.println("employee is present");
-        	daycount=20;
-        	WorkingHours=100;
-        }else if (checkAttendance == partTime) {
-        	System.out.println("employee is working part time");
-        	daycount=10;
-        	WorkingHours=50;
-        }else if(checkAttendance == isAbsent){
-        	System.out.println("employee is absent");
-        }else {
-        	System.out.println("no employee is working in this position");
-        }
-        totalWorkingHours = daycount *  WorkingHours ; 
-        System.out.println(" workingHours:"+WorkingHours);
-        int dailySalary= WorkingHours * salaryPerHour;
-        System.out.println("DailySalary is:"+dailySalary);
-        EmpMonthlySalary = totalWorkingHours * dailySalary ;
-        System.out.println("Employee monthly Salary is :"+ EmpMonthlySalary);
-     }
-  }
+ public class Main {
+		
+		public static void main(String[] args) {
+			EmpWage e = new EmpWage();
+			e.Attendance();
+			Dailywages d = new Dailywages();
+			d.dailywage();
+		}
+	}
+
 
