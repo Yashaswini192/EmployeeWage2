d932d8f2fb5a63861733859a5ddca9a8830d78ff
Yashaswini192 <voruganti.yashaswini@ust.com>
2023-02-17 23:20:49 +0530

    Compute EmployeeWage for Multiple Companies

--- EmpWage.java
+++ EmpWage.java
@@ -4,52 +4,61 @@ public class EmpWage {
 
 	static int isPresent = 1 ;
 	static int isAbsent = 0 ;
-	static int part_time = 2;
-	static int wagePerHour = 20;
-	static int fulldayHours = 20;
-	static int workingDaysPerMonth = 20;
-	static int totalWorkingHours = 0;
-	static int WorkingHours = 0;
+	static final int part_time = 1;
+	static final int full_time = 2;
+	//static int wagePerHour = 20;
+	//static int fulldayHours = 20;
+	//static int workingDaysPerMonth = 2;
+	//static int totalWorkingHours = 0;
+	//static int WorkingHours = 0;
 
-	public void Attendance() {
+	public static int Attendance(String company,int wagePerHour,int totalWorkingHours,int numOfWorkingDays) {
 
-		int checkAttendance = (int) ((Math.random() * 10) % 4);
-		switch (checkAttendance) {
-		case 1:
-			System.out.println("employee is present");
-			WorkingHours = 100;
-			workingDaysPerMonth = 20;
-			break;
-		case 2:
-			System.out.println("employee is absent");
-			WorkingHours = 0;
-			workingDaysPerMonth = 0;
-			break;
-		case 3:
-			System.out.println("employee is working part time");
-			WorkingHours = 50;
-			workingDaysPerMonth = 10;
-			break;
-		default:
-			System.out.println(" employee need to be hired");
-			break;
+		int empHrs =  0,totalEmpHrs = 0, totalWorkingDays =0;
+
+		while(totalEmpHrs <= totalWorkingHours && totalWorkingDays < numOfWorkingDays) {
+
+			totalWorkingDays++;
+			int checkAttendance = (int) Math.floor(Math.random() * 10) % 3;
+			switch (checkAttendance) {
+			case part_time:
+				System.out.println("employee is present");
+				empHrs = 4;
+				//workingDaysPerMonth = 20;
+				break;
+			case full_time:
+				System.out.println("employee is absent");
+				empHrs = 8;
+				//workingDaysPerMonth = 0;
+				break;
+			default:
+				empHrs = 0;
+			}
+			totalEmpHrs += empHrs; 
+			System.out.println("day:" + totalWorkingDays + " Employee hour: " + empHrs);
 		}
+
+
+		int EmpMonthlyWages = totalEmpHrs * wagePerHour ;
+		System.out.println("Employee monthly wages for company: " + company + " is :"+ EmpMonthlyWages);
+		return EmpMonthlyWages;
+
 	}				
 }
 
-class Dailywages extends EmpWage{ //single Inheritance
+/*class Dailywages extends EmpWage{ //single Inheritance
 
 
 	public void dailywage() {
 
-		totalWorkingHours = workingDaysPerMonth *  WorkingHours ; 
-		System.out.println("totalworkingHours:"+totalWorkingHours);
+	//	totalWorkingHours = workingDaysPerMonth *  WorkingHours ; 
+	//	System.out.println("totalworkingHours:"+totalWorkingHours);
 
-		int dailyWages = WorkingHours * wagePerHour ;
+	//	int dailyWages = WorkingHours * wagePerHour ;
 		System.out.println("dailywages:"+dailyWages);
 
 		int MonthlyWages = totalWorkingHours * dailyWages ;
 		System.out.println("Employee monthly wages is :" +MonthlyWages);
 
 	}
-}
+}*/
--- Main.java
+++ Main.java
@@ -3,10 +3,9 @@ package com.bl;
  public class Main {
 		
 		public static void main(String[] args) {
-			EmpWage e = new EmpWage();
-			e.Attendance();
-			Dailywages d = new Dailywages();
-			d.dailywage();
+			EmpWage empwage = new EmpWage();
+			empwage.Attendance("TATA" , 20 ,3 , 10);
+			empwage.Attendance("AGS", 15, 6, 20);
 		}
 	}
 
